744925753c0c04005c8a52b4d4f75b472b52f70e
Maxine <33124096+max1ne@users.noreply.github.com>
2019-06-27 03:49:20 +0800

    Update parse-debug-log.sh

--- parse-debug-log.sh
+++ parse-debug-log.sh
@@ -2,7 +2,7 @@
 clear
 echo Zelnode Major Events (debug.log)
 echo 
-user@BAMF-max:~/.zelcash$ cat  ~/.zelcash/debug.log | egrep -i "Zelnode|Benchmark" | grep -v Ping | grep -v "Got bad" | grep -v "znw - invalid" | tee parse-debug-log.txt
+cat  ~/.zelcash/debug.log | egrep -i "Zelnode|Benchmark" | grep -v Ping | grep -v "Got bad" | grep -v "znw - invalid" | tee parse-debug-log.txt
 rm parse-debug-log.sh
 echo
 echo
