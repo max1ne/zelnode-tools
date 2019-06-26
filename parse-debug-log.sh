de230b0d9a9222d7373b28cfecef1dbe48c68105
Maxine <33124096+max1ne@users.noreply.github.com>
2019-06-27 03:03:45 +0800

    Update 1

--- parse-debug-log.sh
+++ parse-debug-log.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+clear
+echo ** Zelnode Major Events **
+echo  
 cat  ~/.zelcash/debug.log | grep Zelnode | grep -v Ping | grep -v "Got bad" | grep -v "znw - invalid" | tee parse-debug-log.txt
